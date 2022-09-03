d71f11f3221deeed45c32d2b28839d0e8137cf2f
Rist115 <to78314910@gmail.com>
2022-09-03 16:40:31 +0900

    first commit

--- dethub/__init__.py
+++ dethub/__init__.py
@@ -1,2 +1,2 @@
 # flake8: noqa:F401
-from . import datasets, models
+from . import datasets, models, visualization
---
+++ dethub/visualization/vis_backend.py
@@ -0,0 +1,39 @@
+import os
+from typing import Optional
+
+from mmengine.registry import VISBACKENDS
+from mmengine.visualization import WandbVisBackend as Base
+
+
+@VISBACKENDS.register_module(force=True)
+class WandbVisBackend(Base):
+
+    def __init__(self,
+                 save_dir: str,
+                 init_kwargs: Optional[dict] = None,
+                 define_metric_cfg: Optional[dict] = None,
+                 commit: Optional[bool] = True):
+        super().__init__(save_dir)
+        self._init_kwargs = init_kwargs
+        self._define_metric_cfg = define_metric_cfg
+        self._commit = commit
+
+    def _init_env(self):
+        """Setup env for wandb."""
+        if not os.path.exists(self._save_dir):
+            os.makedirs(self._save_dir, exist_ok=True)  # type: ignore
+        if self._init_kwargs is None:
+            self._init_kwargs = {'dir': self._save_dir}
+        else:
+            self._init_kwargs.setdefault('dir', self._save_dir)
+        try:
+            import wandb
+        except ImportError:
+            raise ImportError(
+                'Please run "pip install wandb" to install wandb')
+
+        wandb.init(**self._init_kwargs)
+        if self.define_metric_cfg is not None:
+            for metric, summary in self.define_metric_cfg.items():
+                wandb.define_metric(f'val/{metric}', summary=summary)
+        self._wandb = wandb
