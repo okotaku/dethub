b45bbeac45fee1fd5257671931128869ecf74d4a
Rist115 <to78314910@gmail.com>
2022-09-10 14:58:39 +0900

    first commit

--- tests/test_models/test_detectors/test_single_stage.py
+++ tests/test_models/test_detectors/test_single_stage.py
@@ -8,6 +8,7 @@ from parameterized import parameterized
 
 from mmdet.structures import DetDataSample
 from mmdet.testing import demo_mm_inputs, get_detector_cfg
+from mmdet.testing.utils import _get_config_directory
 from mmdet.utils import register_all_modules
 
 
@@ -20,6 +21,9 @@ class TestSingleStageDetector(TestCase):
         'projects/livecell/yolox/yolox_s_livecell.py',
     ])
     def test_init(self, cfg_file):
+        config_dpath = _get_config_directory(cfg_file)
+        print(config_dpath)
+        kk  # noqa
         model = get_detector_cfg(cfg_file)
         model.backbone.init_cfg = None
 
